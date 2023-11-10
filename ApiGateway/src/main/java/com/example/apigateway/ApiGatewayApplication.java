60ac7fd0eac4b2306b8328e5184c973413623321
Tadporn <88502892+Tadphorn@users.noreply.github.com>
2023-11-10 21:04:48 +0700

    new rankingbook

--- ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.example.rankingbook;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RankingBookApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RankingBookApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
