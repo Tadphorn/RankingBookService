eb406cc21c4e5be7786157279a4573807c5f8793
Tadporn <88502892+Tadphorn@users.noreply.github.com>
2023-11-10 15:09:06 +0700

    ranking

---
+++ RankingBook/src/main/java/com/example/rankingbook/RankingBookApplication.java
@@ -0,0 +1,14 @@
+package com.example.rankingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RankingBookApplication.class, args);
+        System.out.println("start ranking");
+    }
+
+}
